bbf1051de260791a3ec16b16046062313915a302
Jeffrey Hill <jdhill2142@gmail.com>
2023-05-31 12:42:58 -0400

    initial commi

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,38 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/connectDB.js');
+const Task = require('./models/taskModel.js');
+const taskRoutes = require('./routes/taskRoutes.js')
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is runiing on port ${PORT}`)
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors({
+    origin: ['http://localhost:5000/']
+}));
+app.use('/api/tasks', taskRoutes);
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Home page');
 })
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is runnning on port ${PORT}`)
+        });
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+startServer();
