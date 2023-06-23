0424eea70e1ba4c294d7aa395a190992c2337bc7
Jeffrey Hill <65543184+webdevhill@users.noreply.github.com>
2023-06-23 12:11:02 -0400

    Update server.js
    setting cors permissions

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://mern-task-app-1b6x-api.onrender.com");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
